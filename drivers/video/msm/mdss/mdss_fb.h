e44f5e4868ceca5347c220a6ab07fd680018315a
Jeevan Shriram <jshriram@codeaurora.org>
2014-03-27 10:53:40 -0700

    msm: mdss: share fb memory ion fd to userspace
    Add new operation request to share ion fd to userspace
    for mapping to the frame buffer memory.
    
    Change-Id: I404c73c19e3fffec593ffd32c34e7b1a74a500d8
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -246,6 +246,8 @@ struct msm_fb_data_type {
 	struct list_head proc_list;
 	struct ion_client *fb_ion_client;
 	struct ion_handle *fb_ion_handle;
+	struct dma_buf *fbmem_buf;
+
 	u32 wait_for_kickoff;
 	bool mdss_fb_split_stored;
 };
